186c8b89e8c7e44eeeefa527a53c2059f751765e
charlescote1 <charles.cote@uvm.edu>
2024-11-07 20:59:08 -0500

    Update heapSort.h

--- sorting-algorithms/heapSort.h
+++ sorting-algorithms/heapSort.h
@@ -14,24 +14,32 @@ inline int leftChild(const int& i) {
 // i is the index of the value being percolated down
 // n is the number of items in the heap
 template <typename Comparable>
-void percolateDown(vector<Comparable> &items, int& i, unsigned long& n, int& child, Comparable& tmp, unsigned long& reads) {
+void percolateDown(vector<Comparable> &items, int& i, unsigned long& n, int& child, Comparable& tmp, unsigned long& reads, unsigned long& allocations) {
     for(tmp = items[i]; leftChild(i) < n; i = child) {
         child = leftChild(i);
+        ++reads; // reading items[i]
+        allocations += sizeof(tmp);
         // choose the child with the larger value
         if (child != n - 1) {
             if (items[child] < items[child + 1]) {
+                ++reads;
                 ++child;
             }
         }
         // if the parent is less than the child, swap them
+        ++reads;
+        ++reads;
         if (tmp < items[child]) {
             items[i] = items[child];
+            ++reads;
+            allocations += sizeof(items[i]);
         } else {
             // parent is >= both children. nothing more to do.
             break;
         }
     }
     items[i] = tmp;
+    allocations += sizeof(items[i]);
 }
 
 template <typename Comparable>
@@ -43,7 +51,7 @@ vector<Comparable> heapSort(vector<Comparable> items, unsigned long& reads, unsi
     // build the heap (with max value at root)
     for (i = items.size() / 2 - 1; i >= 0; --i) {
         n = items.size();
-        percolateDown(items, i, n, child, tmp, reads);
+        percolateDown(items, i, n, child, tmp, reads, allocations);
     }
 
     // Uncomment this line if you want to see each iteration
@@ -53,12 +61,15 @@ vector<Comparable> heapSort(vector<Comparable> items, unsigned long& reads, unsi
     for (j = items.size() - 1; j > 0; --j) {
         // swap the maximum out
         temp = items[0];
+        ++reads;
         items[0] = items[j];
+        ++reads;
         items[j] = temp;
+        allocations += 3 * sizeof(Comparable);
 
         // make it into a heap again
         i = 0;
-        percolateDown(items, i, j, child, tmp, reads);
+        percolateDown(items, i, j, child, tmp, reads, allocations);
 
         // Uncomment this line if you want to see each iteration
         //printVec(vec);
